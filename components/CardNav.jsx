6d6ae39b0f1fab3569048e90f7128628c755f7b3
Mirza-Shamaaim123 <shamaaimmirza902@gmail.com>
2025-12-01 00:32:31 +0500

    Updated the navbar button

--- components/CardNav.jsx
+++ components/CardNav.jsx
@@ -173,7 +173,7 @@ const CardNav = ({
             type="button"
             className="card-nav-cta-button hidden md:inline-flex border-0 rounded-[calc(0.75rem-0.2rem)] px-4 items-center h-full font-medium cursor-pointer transition-colors duration-300"
             style={{ backgroundColor: buttonBgColor, color: buttonTextColor }}>
-            Get Started
+            READ CV
           </button>
         </div>
 
