cdb012837778e5c7f4fc0eeac86ef76be1ced600
Mirza-Shamaaim123 <shamaaimmirza902@gmail.com>
2025-12-01 13:52:37 +0500

    Fix nested button issue in Read CV modal

--- components/CardNav.jsx
+++ components/CardNav.jsx
@@ -170,12 +170,9 @@ const CardNav = ({
             )}
           </div>
 
-          <button
-            type="button"
-            className="card-nav-cta-button hidden md:inline-flex border-0 rounded-[calc(0.75rem-0.2rem)] px-4 items-center h-full font-medium cursor-pointer transition-colors duration-300"
-            style={{ backgroundColor: buttonBgColor, color: buttonTextColor }}>
-           <ReadCvModal/>
-          </button>
+          {/* Sirf ye use karo, outer button hata do */}
+          <ReadCvModal buttonBgColor="#111" buttonTextColor="#fff" />
+
         </div>
 
         <div
